be0055fc2373eabf7e26b3791c5bc093a9c4a1ed
Dylan Nelson <3587498+dnelson86@users.noreply.github.com>
2013-03-26 16:55:21 -0400

    Added CalcMatch external sort() and match() routines which are multi-threaded and at least 5x faster than their IDL equivalents running on an itc_cluster node. Matching also uses less memory. Added blocked I/O to cosmoLoad, where you can now request specific indices if known and an appropriate read strategy is calculated such that unnecessary data is never read from disk. Used to implement CalcMatchBlock which does the match between two ID arrays by loading the second block by block, reducing the total memory usage to ~the size of the first array.

---
+++ CalcMatch/main.h
@@ -0,0 +1,39 @@
+#ifndef ALLVARS_H
+#define ALLVARS_H
+
+#define VERBOSE
+
+#ifdef VERBOSE
+#define IF_VERBOSE(expr) (expr)
+#else
+#define IF_VERBOSE(expr) ((void)0)
+#endif
+
+// single or double precision
+#ifdef INT64_PRECISION
+typedef long long MyInt;
+typedef int MyIndType; // can change if we need to sort more than max(int) num elements
+#else
+typedef int MyInt;
+typedef int MyIndType;
+#endif
+
+// globals
+MyIndType NumData;
+MyInt *DataIn;
+MyIndType *inds_out;
+MyInt *A, *B;
+	
+// data,index pairs
+struct sort_data
+{
+  MyInt ID;
+  MyIndType index;	
+} *data;
+
+// definitions
+typedef int (*comparer)(const void *, const void *);
+void* my_qsort (void *arr, size_t length, size_t element_size, comparer cmp);
+void _qsort_SD (void *arr, int left, int right, comparer cmp);
+
+#endif
